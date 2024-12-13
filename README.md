203e3dc931c0340938b9c27c5a15edcef2f37388
jurijreact <f888@gmail.com>
2024-12-13 17:44:15 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+npm install express redisnpm install express redis
+
+
+git init
+git remote add origin https://github.com/fotomain/redis1-express.git
+git add .
+git commit -m "first commit"
+git branch -M main
+
+git push -u origin main
---
+++ server.js
@@ -0,0 +1,17 @@
+const redis = require('redis');
+
+(async () => {
+
+    const client = redis.createClient();
+
+    const subscriber = client.duplicate();
+
+    await subscriber.connect();
+
+    await subscriber.subscribe('my_channel', (message) => {
+        console.log("=== message",JSON.parse(message))
+    });
+
+    console.log("=== started")
+
+})();
