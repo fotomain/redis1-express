8c5ca0cd4db77fc3914be9cd42106cdb51ea5e0f
jurijreact <f888@gmail.com>
2024-12-13 21:36:53 +0200

    111

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //CLIENT https://localhost/php-practice/redis-subscribe1.php
 
 const PORT = 3100;
+const HOST_REDIS = "redis://localhost:6379";
 
 var {createServer} = require('http');
 var {Server} = require('socket.io')
@@ -10,6 +11,7 @@ var {createClient} = require('redis')
 var {createAdapter} = require('@socket.io/redis-adapter')
 
 var express = require('express');
+const redisAdapter = require("redis");
 var app = express();
 const server = createServer(app);
 
@@ -24,7 +26,6 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
     console.log('=========== A new client connected');
 
-    // Handle socket events here
 
     socket.on('disconnect', () => {
         console.log('===========   Client disconnected');
@@ -32,12 +33,19 @@ io.on('connection', (socket) => {
 });
 
 (async () => {
-    const pubClient = createClient({ url: "redis://localhost:6379" });  // Add your Redis URL here
+    const pubClient = createClient({ url: HOST_REDIS });  // Add your Redis URL here
     const subClient = pubClient.duplicate();
 
     await Promise.all([pubClient.connect(),subClient.connect()]);
 
     io.adapter(createAdapter(pubClient, subClient));
+
+    subClient.subscribe('my_channel',
+        (params) => {
+            console.log("=== message incomed ",params)
+            io.emit("messageTo1",params)
+        });
+
 
     server.listen(PORT, () => {
         console.log('Server listening on port '+PORT);
